2babd9096faf01b95b806d745648a0e401f05f17
mhnj7 <33918731+mhnj7@users.noreply.github.com>
2021-04-28 23:30:03 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// Test Line 1
+// Test Line 2
 
 console.log(`process.env.SERVER = ${process.env.SERVER}`);
 // get the environment variable, but default to localhost:8082 if its not set
